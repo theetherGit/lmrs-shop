69472005af66b677b131d0de021d6f77f9dcc728
theetherGit <meenashivam9650@gmail.com>
2026-03-28 08:50:17 +0530

    underline tabs amd jsrepo config

--- jsrepo.config.ts
+++ jsrepo.config.ts
@@ -5,11 +5,11 @@ export default defineConfig({
     registries: ["@ieedan/shadcn-svelte-extras"],
     // configure where stuff goes here
     paths: {
-        ui: "$lib/components/ui",
-        block: "$lib/components/block",
-        hook: "$lib/helpers/hooks",
-        action: "$lib/helpers/actions",
-        util: "$lib/helpers/shadcn",
-        lib: "$lib",
+        ui: '$lib/components/ui',
+        block: '$lib/components/block',
+        hook: '$lib/helpers/hooks',
+        action: '$lib/helpers/actions',
+        util: '$lib/helpers/shadcn',
+        lib: '$lib',
     },
 });
